4e33cf3e64bad0b6fbb33c9b9d9c3d730b3db6bd
zenglenn42 <gsnospampls@pobox.com>
2019-10-07 14:34:30 -0500

    adding working Word constructor with unit tests

--- Word.js
+++ Word.js
@@ -1,8 +1,8 @@
 /*
-Contains a constructor, Word, that depends on the Letter constructor. 
+Contains a 'Word' constructor that depends on the Letter constructor. 
 
 This is used to create an object representing the current word the user is attempting to guess. 
-That means the constructor should define:
+That means the constructor defines:
 
   * An array of `new` Letter objects representing the letters of the underlying word
 
@@ -16,4 +16,62 @@ That means the constructor should define:
 
 var Letter = require('./Letter')
 
-console.log(Letter)
+function Word(guessWord) {
+  this.word = guessWord
+  this.letters = this.word.split('').map(letter => new Letter(letter))
+  this.toString = () => this.letters.map(lObj => lObj.toString()).join(' ')
+  this.guessLetter = (letter) => this.letters.map(lObj => lObj.checkLetter(letter))
+}
+module.exports = Word
+
+function UnitTest() {
+  const testData = [
+    {
+      desc: "Check for 'l' in 'hello'",
+      word: "hello",
+      guessedLetter: "l",
+      expectString: "_ _ l l _",
+      expect: "pass"
+    },
+    {
+      desc: "Check for 'a' in 'hello'",
+      word: "hello",
+      guessedLetter: "a",
+      expectString: "_ _ _ _ _",
+      expect: "fail"  // This is a negative test case.  We expect failure.
+    },
+    {
+      desc: "Check for 'a' in 'A'",
+      word: "",
+      guessedLetter: "a",
+      expectString: "_",
+      expect: "fail"  // Fail since test is case-sensitive.
+    }
+  ]
+
+  console.log()
+  let testBanner = "Unit testing 'Word' constructor"
+  let underLines = "-".repeat(testBanner.length)
+  console.log(testBanner)
+  console.log(underLines)
+
+  let nPassed = 0;
+  let nFailed = 0;
+  testData.map((testCase, index) => {
+    process.stdout.write(`${index + 1}. ${testCase.desc}  `)
+    let w = new Word(testCase.word)
+    w.guessLetter(testCase.guessedLetter)
+    process.stdout.write(`[${w.toString()}] `)
+    if (w.toString() === testCase.expectString) {
+      nPassed++
+      let results = (testCase.expect === "pass") ? "PASS" : "PASS (we expected a failure and got it)" 
+      console.log(results)
+    } else {
+      nFailed++
+      console.log("FAIL")
+    }
+  })
+  console.log(`\nTest results: ${nPassed} passed, ${nFailed} failed\n`)
+}
+
+UnitTest()
